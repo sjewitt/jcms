4b9fcbf3fba02235be411eb6c9637c0cfbd76d04
silas <silas@Fluxx>
2018-09-10 19:29:36 +0100

    added Lou templates

---
+++ lou/js/engine.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var engine = {
+    init : function(flag){
+        switch(flag){
+            case 'index':
+                break;
+                
+            case 'about':
+                break;
+        }
+        
+    },
+    
+    
+};
+
