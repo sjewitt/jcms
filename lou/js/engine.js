870b1dc8b39ae16c5f574aea2f2d7fcee806df57
Silas <silas@taxonomics.co.uk>
2018-10-25 16:18:47 +0100

    removed owl carousel

--- lou/js/engine.js
+++ lou/js/engine.js
@@ -56,6 +56,8 @@ var engine = {
                     e.preventDefault();
                 });
 
+                this.getCMSFeed();
+
                 break;
                 
             case 'about':
@@ -63,6 +65,48 @@ var engine = {
         }
         
     },
+    
+    getCMSFeed : function(){
+        console.log("getting RSS content");
+         $.ajax({
+             type:"GET",
+             crossDomain :true,
+//            url:"/cms/feed",
+              url:"/cms/feed/",
+//              url:"http://www.themidge.co.uk/cms/feed",
+//              url:"http://www.themidge.co.uk/cms/feed/",
+//              url:"https://www.themidge.co.uk/cms/feed",
+//              url:"https://www.themidge.co.uk/cms/feed/",
+//            url:"/static/feed.rss",
+            success : function(data){
+                $xml = $(data);
+                var xml = $xml[0];
+                var items = xml.getElementsByTagName('item');
+                for(var a=0;a<items.length;a++){
+                    if($('body').attr('data-wp-content') === items[a].getElementsByTagName('guid')[0].childNodes[0].nodeValue){
+                        var stuff = items[a].getElementsByTagName('content:encoded')[0].childNodes[0].nodeValue;
+                        /*
+						console.log(stuff);
+                        
+                            because of CF, the WP asset urls are causing CORS errors (WP base URL is http: and changing to https: causes 
+                            a redirect loop - I needed to edit the WP DB directly to get back into the admin page) 
+                        
+                        Therefore, pre-process the asset links found:
+                        
+                        var newstuff = stuff.replace(/http/gi,'https');
+                        console.log(newstuff);
+						*/
+                        $("div.bodycontent").html(stuff);
+                    }
+                }
+            },
+            error: function(xhr, status, error) {
+//                var err = eval("(" + xhr.responseText + ")");
+//                alert(err.message);
+            }
+        });
+    },
+    
     switchyShifyThingie : function(){
         console.log('');
     }
